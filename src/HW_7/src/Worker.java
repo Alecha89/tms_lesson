83e4bf10353bfe5b38c340ed7cb19915415609f8
Alecha89 <80847805+Alecha89@users.noreply.github.com>
2026-03-23 19:26:59 +0300

    Revert "HW_7"

--- src/HW_7/src/Worker.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Yushko Aliaksei on 22.03.2026
- */
-public class Worker implements Position{
-    @Override
-    public void printPosition() {
-        System.out.println("Рабочий");
-    }
-}
