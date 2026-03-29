d667ebd480b207a8f744a3f7368cad0932c66a0a
Alecha89 <Alecha89@yandex.ru>
2026-03-29 21:55:04 +0300

    HW_8_1

--- src/HW_8/src/Main.java
+++ src/HW_8/src/Main.java
@@ -2,20 +2,23 @@
  * Created by Yushko Aliaksei on 29.03.2026
  *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int kod;
+        System.out.print("Введите код плана лечения: ");
+        if (scanner.hasNextInt()) {
+            kod = scanner.nextInt();
+            Patient patient = new Patient();
+            Clinic clinic = new Clinic();
 
-        Patient patient = new Patient();
-        Clinic clinic = new Clinic();
-
-        patient.setPlanLecheniya(1);
-        clinic.nameDoctor(patient);
-
-        patient.setPlanLecheniya(2);
-        clinic.nameDoctor(patient);
-
-        patient.setPlanLecheniya(9);
-        clinic.nameDoctor(patient);
-
+            patient.setPlanLecheniya(kod);
+            clinic.nameDoctor(patient);
+        } else {
+            System.out.println("Ошибка: введено не число. Попробуйте снова.");
+            scanner.next();
+        }
     }
 }
